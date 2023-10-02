08f7b951e20b58e8a56f21544b532760c3ffaafb
Hoang Long <longbh@meta-technology.com.vn>
2023-10-02 20:47:08 +0700

    updated

--- server.js
+++ server.js
@@ -47,8 +47,12 @@ mongoose
   });
 
 //middleware
-
-app.use(cors());
+app.use(
+  cors({
+    origin: true, 
+    credentials: true,
+  })
+);
 app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
